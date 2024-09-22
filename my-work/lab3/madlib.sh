dc829dbcd7af210ed5437b1b127ce9655a838f22
alkalink1 <143008344+alkalink1@users.noreply.github.com>
2024-09-22 20:32:04 +0000

    madlib commit

---
+++ my-work/lab3/madlib.sh
@@ -0,0 +1,17 @@
+#! 
+
+clear
+echo "Let's build a mad-lib!"
+
+read -p "1. Name an unusual activity: " NOUN1
+read -p "1. Name a dinner food: " NOUN2
+read -p "1. Name an adjective: " ADJECTIVE3
+read -p "1. Name a mean adjective: " ADJECTIVE4
+read -p "1. Name an animal: " ANIMAL5
+read -p "1. Name a color: " COLOR6
+read -p "1. Name an accessory: " ACCESSORY7
+read -p "1. Name an adjective: " ADJECTIVE8
+
+echo "Once upon a time in a far off land called Charlottesville, there was a girl who loved to $NOUN1. Every day, she woke up, ate her $NOUN2 for breakfast, and went off to her niche and $ADJECTIVE3 hobby. While others thought she was $ADJECTIVE4, she didn't care. She loved it anyways. She always rode her $ANIMAL5 wherever she went, and carried around a $COLOR6 $ACCESSORY7. She taught me and  everyone around her to not care what others think, because her life kept her happy and $ADJECTIVE8."
+
+
